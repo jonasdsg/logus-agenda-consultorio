135e82038357797bae7fe958977ccdf104ebe876
Jonas Góes <jonasdossantos1991@gmail.com>
2021-08-21 13:00:48 -0300

    alteração na superclasse backend service

--- consultorio-front/src/app/componentes/BackendService.model.ts
+++ consultorio-front/src/app/componentes/BackendService.model.ts
@@ -1,9 +1,11 @@
-import { HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 const BACKEND_BASE_URL = 'http://localhost:8080';
 
-export class BackendService {
+export class BackendService <T> {
 
-    constructor(private path:string){
+    constructor(
+        private path:string,
+        private http:HttpClient){
         this.setPath = path;
     }
 
@@ -21,5 +23,21 @@ export class BackendService {
 
     get baseUrl() {
         return `${BACKEND_BASE_URL}/${this.path}`;
+    }
+
+    findAll(){
+        return this.http.get(this.baseUrl,this.getOptions);
+    }
+
+    findByFilters(params){
+        let options = {
+            headers: this.getOptions.headers,
+            params:params
+        }
+        return this.http.get(this.baseUrl,options)
+    }
+
+    save(consulta: T){
+        return this.http.post(this.baseUrl,consulta,this.getOptions);
     }
 }
--- consultorio-front/src/app/componentes/consulta/consulta.service.ts
+++ consultorio-front/src/app/componentes/consulta/consulta.service.ts
@@ -7,25 +7,8 @@ import { BackendService } from './../BackendService.model';
 @Injectable({
     providedIn: 'root'
 })
-export class ConsultaService extends BackendService {
-
-    constructor(private http: HttpClient) {
-        super('consulta');
-    }
-
-    save(consulta: Consulta){
-        return this.http.post(this.baseUrl,consulta,this.getOptions);
-    }
-
-    findAll(){
-        return this.http.get(this.baseUrl,this.getOptions);
-    }
-
-    findByFilters(params){
-        let options = {
-            headers: this.getOptions.headers,
-            params:params
-        }
-        return this.http.get(this.baseUrl,options)
+export class ConsultaService extends BackendService<Consulta> {
+    constructor(http:HttpClient){
+        super('consulta',http);
     }
 }

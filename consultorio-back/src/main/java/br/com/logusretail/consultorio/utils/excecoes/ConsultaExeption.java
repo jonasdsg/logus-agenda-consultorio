53804f81c234e17bd699413afd632923d5b0ffb5
Jonas Góes <jonasdossantos1991@gmail.com>
2021-08-22 21:42:48 -0300

    criada classes de exceções

---
+++ consultorio-back/src/main/java/br/com/logusretail/consultorio/utils/excecoes/ConsultaExeption.java
@@ -0,0 +1,10 @@
+package br.com.logusretail.consultorio.utils.excecoes;
+
+public class ConsultaExeption extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	public ConsultaExeption(String string) {
+		super(string);
+	}	
+
+}
---
+++ consultorio-back/src/main/java/br/com/logusretail/consultorio/utils/excecoes/MedicoException.java
@@ -0,0 +1,10 @@
+package br.com.logusretail.consultorio.utils.excecoes;
+
+public class MedicoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	public MedicoException(String string) {
+		super(string);
+	}
+
+}
